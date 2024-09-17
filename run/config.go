87b6eb2e95a8858fe5d922d1b15e4d66995891b2
Vlad Veselov <orlangur@users.noreply.github.com>
2024-09-18 00:10:02 +0300

    whitespaces

--- run/config.go
+++ run/config.go
@@ -48,7 +48,7 @@ type Config struct {
 	// General application behavior
 	ConfigFile      *cfgutil.ExplicitString `short:"C" long:"configfile" description:"Path to configuration file"`
 	ShowVersion     bool                    `short:"V" long:"version" description:"Display version information and exit"`
-        Create          bool                    `long:"create" description:"Create the wallet if it does not exist"`
+	Create          bool                    `long:"create" description:"Create the wallet if it does not exist"`
 	CreateTemp      bool                    `long:"createtemp" description:"Create a temporary simulation wallet (pass=password) in the data directory indicated; must call with --datadir"`
 	AppDataDir      *cfgutil.ExplicitString `short:"A" long:"appdata" description:"Application data directory for wallet config, databases and logs"`
 	Regtest         bool                    `long:"regtest" description:"Use the test Bitcoin regtest network (default mainnet)"`
