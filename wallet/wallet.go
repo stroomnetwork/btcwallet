9f9a0b462987fc35f137f6e11f8f2ebd99486f1f
dpiatkivskyi <dmytro.piatkikvskyi@gmailcom>
2024-12-02 18:07:17 +0200

    enables back double spend check

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -3509,7 +3509,7 @@ func (w *Wallet) SendOutputsWithDataAndRedeemIdCheck(outputs []*wire.TxOut, keyS
 	coinSelectionStrategy CoinSelectionStrategy, label string, redeemId uint32, start, end *BlockIdentifier, data []byte) (*wire.MsgTx,
 	error) {
 
-	/*isSpent, hash, err := w.IsRedeemIdAlreadySpent(redeemId, start, end)
+	isSpent, hash, err := w.IsRedeemIdAlreadySpent(redeemId, start, end)
 
 	if err != nil {
 		return nil, err
@@ -3517,7 +3517,7 @@ func (w *Wallet) SendOutputsWithDataAndRedeemIdCheck(outputs []*wire.TxOut, keyS
 
 	if isSpent {
 		return nil, fmt.Errorf("redeem id %d already spent in tx %s", redeemId, hash)
-	}*/
+	}
 
 	return w.sendOutputs(
 		outputs, keyScope, account, minconf, satPerKb,
