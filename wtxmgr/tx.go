a2fc52bdb9cb6521210731732399a9104a5e1451
Oleg Bondar <bullet-tooth@users.noreply.github.com>
2025-01-24 11:42:46 +0000

    Revert "fix: rollback state (#64)"
    This reverts commit 8150e1d5b100c2dd2b1115ea360c45394976f560.

--- wtxmgr/tx.go
+++ wtxmgr/tx.go
@@ -10,7 +10,6 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/btcsuite/btcd/blockchain"
@@ -557,8 +556,7 @@ func (s *Store) Rollback(ns walletdb.ReadWriteBucket, height int32) error {
 
 func (s *Store) rollback(ns walletdb.ReadWriteBucket, height int32) error {
 	minedBalance, err := fetchMinedBalance(ns)
-	// NB: workaround to rollback broken state
-	if err != nil && !strings.Contains(err.Error(), "read 0") {
+	if err != nil {
 		return err
 	}
 
